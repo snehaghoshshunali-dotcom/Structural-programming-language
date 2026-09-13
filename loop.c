021d7f7551c021a2b8e53a5c6222ea5dc78ecc84
Sneha Ghosh <snehaghoshshunali@gmail.com>
2026-09-13 20:15:33 +0600

    Add C program files

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int dice =1;
+    while (dice<=6){
+        if (dice<6){
+            printf("No Yatzy\n");
+        }
+        else{
+            printf("Yatzy!\n");
+        }
+        dice = dice + 1;
+    }
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+    int sum=0;
+    int i;
+    for (i=1;i<=5;i++){
+        sum=sum+i;
+    }
+    printf("sum is %d\n", sum);
+    return 0;
+
+
+
+
+}
